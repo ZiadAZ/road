716215591ef91ef962d8e67b2369007e8a34e318
Ziad <ziadzayed7@gmail.com>
2022-01-11 20:50:29 +0300

    Git 3 Done

--- road.py
+++ road.py
@@ -1,20 +1,37 @@
 # -*- coding: utf-8 -*-
 """
-Ask the user to enter their first name and then display the length of their name.
- upload program file to GitHub and past the link.
+Ask how many people the user wants to invite to a party.
+If they enter a number below10, 
+ask for the names and after each name display “[name] has been invited”. 
+If they enter a number which is 10 or higher, display the message “Too many people”. 
+Upload program to GitHub and write the link?
 """
 
-def enter(msg='Enter Your first name'):
+def enter(msg='How many people the user wants to invite?'):
    print(msg)
-   return input()
+   x= input()
+   if(x.isnumeric()):
+       x=int(x)
+       if x<10:
+         names=[]
+         while x>0:
+             x-=1
+             print('enter name '+str(len(names)+1))
+             names.append(input())
+         return names
+       
+       else:
+           return enter(' ““Too many people”')
+   else:
+       return enter('Erorr!  Enter a number corectly: ')
 
 
 def start():
 
- 
-  
-   f=enter()
-   print('Your name is: '+f+"\n and it's lenth is: "+str(len(f) ))
+  for name in enter():
+      print(name+' has been invited')
+
+
    
 
    
