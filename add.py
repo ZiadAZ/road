17e0db55259c833087f04c23f722b06981e91426
Ziad <ziadzayed7@gmail.com>
2022-01-13 17:02:48 +0300

    add with (*x) done

---
+++ add.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Write sub program add two or three numbers? upload the code to GitHub, 
+call will be like that *
+
+"""
+
+def add(*x):
+    return sum(x)
+
+
+
+
+print(add(3,4))
+print(add(1,3,5))
